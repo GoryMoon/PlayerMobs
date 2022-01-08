29c7f39d7aed96937b39223920c4a1dc68dac09c
Gustaf Järgren <gurreja@gmail.com>
2022-01-08 21:27:43 +0100

    Move to use ForgeSpawnEggItem to fix obfuscation crash (#15)

--- src/main/java/se/gory_moon/player_mobs/Constants.java
+++ src/main/java/se/gory_moon/player_mobs/Constants.java
@@ -5,6 +5,7 @@ public class Constants {
     public static final String MOD_ID = "player_mobs";
 
     public static final String PLAYER_MOB_ENTITY = "player_mob";
+    public static final String PLAYER_MOB_SPAWN_EGG = PLAYER_MOB_ENTITY + "_spawn_egg";
 
     public static final String SOUND_PLAYER_MOB_AMBIENT = "player_mob.ambient";
     public static final String SOUND_PLAYER_MOB_HURT = "player_mob.hurt";
